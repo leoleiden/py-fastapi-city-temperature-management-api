47dad8ed7f660f89cf5b80587f1e0be07ac77037
Leonid Lachmann <148564629+leoleiden@users.noreply.github.com>
2025-06-17 14:54:36 +0300

    import Optional

--- app/routers/temperatures.py
+++ app/routers/temperatures.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
-from typing import List
+from typing import Optional, List
 from ..services import weather
 from .. import database, models, schemas, crud
 
