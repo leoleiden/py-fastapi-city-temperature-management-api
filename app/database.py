ae188b876783c914ae8cc7ef482c737812d74c1c
Leonid Lachmann <148564629+leoleiden@users.noreply.github.com>
2025-06-17 14:15:17 +0300

    delete_legacy_declarative_base

--- app/database.py
+++ app/database.py
@@ -1,11 +1,7 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
-from sqlalchemy.ext.declarative import declarative_base as legacy_declarative_base
 
-try:
-    Base = declarative_base()
-except Exception:
-    Base = legacy_declarative_base()
+Base = declarative_base()
 
 DATABASE_URL = "sqlite+aiosqlite:///./cities.db"
 
